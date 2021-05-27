28bfdf3cfce0703319c09e3302184ab20681bdc0
rliberty <roryliberty@gmail.com>
2021-05-27 15:21:24 -0500

    save

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,10 @@
-import { Component, OnInit} from '@angular/core';
-
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-import { MessageService} from '../message.service';
-
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
 export class HeroesComponent implements OnInit {
-
-  // NOT instantiated variable
-  selectedHero?: Hero;
-
-  // Instantiated variable
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log(hero.id);
-    this.messageService.add(`HeroesComponent: Selected hero id=${ hero.id }`);
   }
 
   getHeroes(): void {
@@ -34,14 +12,3 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 }
-
-/*
-public
-private
-protected
- */
-
-/*
-static
-readonly
- */
